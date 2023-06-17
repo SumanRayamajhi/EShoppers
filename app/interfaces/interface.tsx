ec13299d2f5781b24046ec34099d06c483a371ff
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2023-06-17 20:21:52 +0200

    some chages with navbar with test

--- app/interfaces/interface.tsx
+++ app/interfaces/interface.tsx
@@ -1,12 +1,3 @@
 export interface NavbarProps {
   image?: string;
 }
-
-export interface NavCatagoriesProps {
-  open: boolean;
-  setOpen: boolean;
-  links: {
-    to: string;
-    name: string;
-  }[];
-}

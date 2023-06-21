693f685b9de699e56d03c7f4e33bd40844cdde4b
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2023-06-21 18:19:06 +0200

    add cards to catagories pages

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import posts from "../data";
+import ProductCard from "../components/product/product-card";
 
 function Books() {
+  const books = posts.filter((post) => post.catagories === "books");
   return (
-    <div className="container mx-auto py-32 px-8">
-      <h1>Books</h1>
+    <div className="container mx-auto py-32 px-8 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6">
+      {books.map((books) => (
+        <div key={books.id}>
+          <ProductCard
+            title={books.title}
+            image={books.image}
+            price={books.price}
+            description={books.desc}
+          />
+        </div>
+      ))}
     </div>
   );
 }
--- app/fitness/page.tsx
+++ app/fitness/page.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import posts from "../data";
+import ProductCard from "../components/product/product-card";
 function Fitness() {
+  const fitness = posts.filter((post) => post.catagories === "fitness");
   return (
-    <div className="container mx-auto py-32 px-8">
-      <h1> Fitness</h1>
+    <div className="container mx-auto py-32 px-8 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6">
+      {fitness.map((fitness) => (
+        <div key={fitness.id}>
+          <ProductCard
+            title={fitness.title}
+            image={fitness.image}
+            price={fitness.price}
+            description={fitness.desc}
+          />
+        </div>
+      ))}
     </div>
   );
 }

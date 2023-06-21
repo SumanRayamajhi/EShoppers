d50758a97e8bf85c830e5f35f4c40e450fb85aad
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2023-06-21 17:58:21 +0200

    remove error from navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,70 +1,47 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import NavbarCatagories from "./NavbarCatagories";
 import { HiMenu, HiX, HiShoppingCart } from "react-icons/hi";
 import Link from "next/link";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
-  const [color, setColor] = useState("transparent");
-  const [textColor, setTextColor] = useState("white");
 
   const onClickMenuButton = () => {
     setOpen((prev) => !prev);
   };
 
-  useEffect(() => {
-    const changeColor = () => {
-      if (window.scrollY >= 90) {
-        setColor("#000");
-        setTextColor("#fff");
-      } else {
-        setColor("transparent");
-        setTextColor("#000");
-      }
-    };
-    window.addEventListener("scroll", changeColor);
-  }, []);
-
   return (
-    <header
-      style={{ backgroundColor: `${color}` }}
-      className="fixed left-0 top-0 w-full z-10 ease-out duration-300 px-5"
-    >
-      <nav className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
+    <header className="fixed bg-teal-700 left-0 top-0 w-full z-10 px-5">
+      <nav className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-black">
         <div onClick={onClickMenuButton} className="block sm:hidden z-10">
           {open ? (
-            <HiX size={30} style={{ color: `${textColor}` }} />
+            <HiX size={30} className="text-white" />
           ) : (
-            <HiMenu size={30} style={{ color: `${textColor}` }} />
+            <HiMenu size={30} className="text-white" />
           )}
         </div>
 
         <Link href="/">
-          <h1 style={{ color: `${textColor}` }} className="font-bold text-4xl">
-            LOGO
-          </h1>
+          <h1 className="font-bold text-white text-4xl">LOGO</h1>
         </Link>
-        <div
-          className="hidden sm:flex font-bold"
-          style={{ color: `${textColor}` }}
-        >
-          <NavbarCatagories open={open} onClickMenuButton={onClickMenuButton} />
+        <div className="hidden sm:flex font-bold text-white">
+          <NavbarCatagories onClickMenuButton={onClickMenuButton} />
         </div>
         <div
           className={
             open
-              ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
-              : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
+              ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-teal-700 text-center ease-in duration-300"
+              : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-teal-700 text-center ease-in duration-300"
           }
         >
-          <NavbarCatagories open={open} onClickMenuButton={onClickMenuButton} />
+          <NavbarCatagories onClickMenuButton={onClickMenuButton} />
         </div>
         <div className="relative items-center ">
           <span className="absolute flex justify-center items-center text-center bg-amber-600 bottom-4 left-5 h-5 w-5  rounded-full font-semibold">
             0
           </span>
-          <HiShoppingCart style={{ color: `${textColor}` }} size={25} />
+          <HiShoppingCart className="text-white" size={25} />
         </div>
       </nav>
     </header>
--- app/components/navbar/NavbarCatagories.tsx
+++ app/components/navbar/NavbarCatagories.tsx
@@ -3,21 +3,19 @@ import React from "react";
 import { links } from "./Nav-links";
 
 interface NavCatagoriesProps {
-  open: boolean;
   onClickMenuButton: () => void;
 }
 
 const NavbarCatagories: React.FC<NavCatagoriesProps> = ({
-  open,
   onClickMenuButton,
 }) => {
   return (
     <div>
-      <ul className="md:flex flex-row md:text-2xl text-4xl ">
+      <ul className="md:flex flex-row md:text-2xl text-4xl text-white ">
         {links.map((link) => (
           <li
             key={link.name}
-            className="p-6 md:py-2 md:px-4 hover:bg-teal-100 hover:text-gray-800 hover:rounded-md ease-out duration-300"
+            className="p-6 font-bold md:py-2 md:px-4 hover:bg-teal-600 hover:text-gray-800 hover:rounded-md hover:ease-out hover:duration-300"
           >
             <Link href={link.to} onClick={onClickMenuButton}>
               {link.name}
